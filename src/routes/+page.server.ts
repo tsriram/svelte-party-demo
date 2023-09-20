4508e9dc9c80ae377a2f58ef0707d5ddabbba30b
Sriram Thiagarajan <sri.sjc@gmail.com>
2023-09-20 13:28:29 +0530

    debug cloudflare pages error

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,9 @@ import { PUBLIC_PARTYKIT_HOST, PUBLIC_PARTYKIT_ROOM } from '$env/static/public';
 export const load: PageServerLoad = async ({ fetch }) => {
 	const host = PUBLIC_PARTYKIT_HOST;
 	const room = PUBLIC_PARTYKIT_ROOM;
-	const response = await fetch(`${host}/party/${room}`);
+	const partykitUrl = `${host}/party/${room}`;
+	console.log('partykitUrl: ', partykitUrl);
+	const response = await fetch(partykitUrl);
 	const data = await response.json();
 	return {
 		count: data?.count || 0
