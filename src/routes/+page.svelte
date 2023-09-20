39425d85e6d967b3bc74c874348ce7e3cfb18878
Sriram Thiagarajan <sri.sjc@gmail.com>
2023-09-20 16:55:17 +0530

    remove unused imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
